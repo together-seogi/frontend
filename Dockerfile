2a32b0a521d3277aa6359b835e066298db047522
Juan Lee <ninejuan@protonmail.com>
2025-02-25 02:45:03 +0900

    build: dockerfile changed

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:20.10.0-alpine
+
+WORKDIR /app
+
+# Install dependencies only when needed
+COPY package.json yarn.lock ./
+RUN yarn install
+
+# Copy all files
+COPY . .
+
+# Build the Next.js application
+RUN yarn build
+
+# Expose the port the app runs on
+EXPOSE 3000
+
+# Start the application
+CMD ["yarn", "run", "preview"]
+
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    port: 3000,
+    strictPort: true,
+    host: "0.0.0.0",
+  },
+  server: {
+    cors: {
+      origin: [
+        "https://tseogi.juany.kr",
+        "https://api-tseogi.juany.kr",
+      ],
+      methods: ["POST", "GET", "PATCH", "PUT", "OPTIONS", "DELETE"],
+      allowedHeaders: ["*"],
+    }
+  },
+});
